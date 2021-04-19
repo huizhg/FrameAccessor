b5a4895a950708c8837f3296201ed3bf1d7796d4
Hui Zhang <mrc19hzg@cs.umu.se>
2021-04-19 15:32:15 +0200

    Invoke the closeAccessor() method in the getPerformanceStatics(). update the statistics when the get method is called.

--- src/main/java/MyFrameAccessor.java
+++ src/main/java/MyFrameAccessor.java
@@ -28,7 +28,7 @@ public class MyFrameAccessor implements FrameAccessor {
             performance.put(client.getHost(),new ServiceRecorder());
         }
         performanceStatistics = new ImpPerformanceStatics(performance);
-        executorService = new ThreadPoolExecutor(clients.length*50,clients.length*50, 60L,TimeUnit.SECONDS,new LinkedBlockingDeque<Runnable>());
+        executorService = new ThreadPoolExecutor(clients.length*10,clients.length*10, 1L,TimeUnit.SECONDS,new LinkedBlockingDeque<Runnable>());
         //executorService = Executors.newSingleThreadExecutor();
         startTime = System.currentTimeMillis();
     }
@@ -104,10 +104,6 @@ public class MyFrameAccessor implements FrameAccessor {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        long endTime = System.currentTimeMillis();
-        long runningTime = endTime -startTime;
-        performanceStatistics.update(runningTime,framesCount);
-
     }
 
     /*
@@ -177,6 +173,7 @@ public class MyFrameAccessor implements FrameAccessor {
                 int latency = (int) (t2-t1);
                 System.out.printf("Received block %d, %d\n",blockX,blockY);
                 serviceRecorder.addLatency(latency);
+
                 return block;
             } catch (SocketTimeoutException e) {
                 serviceRecorder.incDropCount();
@@ -191,6 +188,11 @@ public class MyFrameAccessor implements FrameAccessor {
 
 
     public PerformanceStatistics getPerformanceStatistics() {
+
+        this.closeAccessor();
+        long endTime = System.currentTimeMillis();
+        long runningTime = endTime -startTime;
+        performanceStatistics.update(runningTime,framesCount);
         return this.performanceStatistics;
     }
 
@@ -229,7 +231,7 @@ public class MyFrameAccessor implements FrameAccessor {
 
     public static class ImpFactory implements Factory{
 
-        public FrameAccessor getFrameAccessor(StreamServiceClient streamServiceClient, String streamName) {
+        public MyFrameAccessor getFrameAccessor(StreamServiceClient streamServiceClient, String streamName) {
             StreamServiceClient[] clients = new StreamServiceClient[]{streamServiceClient};
             try {
                  StreamInfo[] streamInfos = clients[0].listStreams();
@@ -247,7 +249,7 @@ public class MyFrameAccessor implements FrameAccessor {
 
         }
 
-        public FrameAccessor getFrameAccessor(StreamServiceClient[] streamServiceClients, String streamName) {
+        public MyFrameAccessor getFrameAccessor(StreamServiceClient[] streamServiceClients, String streamName) {
             try {
                 StreamInfo[] streamInfos = streamServiceClients[0].listStreams();
                 for (StreamInfo eachStream : streamInfos) {
